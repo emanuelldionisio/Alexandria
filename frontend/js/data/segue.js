2d24bf7400fa4bf9d4d0cdc18cb3a4197644117f
emanuelldionisio <emanuel.eduardo@academico.ifpb.edu.br>
2025-05-03 16:34:27 -0300

    Prosegui o js do menu, adicionando as funcoes para mudar a foto, o nome, e as tags

--- frontend/js/data/segue.js
+++ frontend/js/data/segue.js
@@ -3,8 +3,4 @@ export const segue = [
         "seguinte": 1,
         "seguido": 2
     },
-    {
-        "seguinte": 2,
-        "seguido": 1
-    }
 ]
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,17 +1,46 @@
-
+import { usuario } from './data/usuario.js';
 import { segue } from './data/segue.js';
+import { palavra_usuario } from './data/palavra_usuario.js';
 
 function carregarMenu() {
-    let id = 1;
-    console.log(id);
+    let id = 2;
+    
+    const coresBootstrap = [
+        'primary', 'secondary', 'success', 'danger',
+        'warning', 'info', 'dark'
+    ];
 
+    //Ajustar a quantidade de seguidores
     let container_seguidores = document.getElementById("menu-usuario__informacoes__seguidores");
     container_seguidores.innerHTML = segue.filter(obj => obj["seguido"] == id).length + " seguidores";
 
     let container_segue = document.getElementById("menu-usuario__informacoes__seguindo");
     container_segue.innerHTML = segue.filter(obj => obj["seguinte"] == id).length + " seguindo";
 
+    //Adicionar a foto de perfil
+    let img_fotodeperfil = document.getElementById("foto-de-perfil");
+    img_fotodeperfil.src = `imgs/usuario/${id}.png`;
 
+    //Adicionar o nome do user
+    let mensagem_boasvindas = document.getElementById("menu-usuario__mensagem");
+    mensagem_boasvindas.innerHTML = `Olá, ${usuario.find(obj => obj.cod == id).nome}`;
+
+    //Adicionar as palavras chave
+    let palavras = palavra_usuario.filter(obj => obj.usuario == id);
+    let menu_palavras = document.getElementById("menu-palavras-chave");
+    for (let palavra of palavras) {
+        let corAleatoria = coresBootstrap[Math.floor(Math.random() * coresBootstrap.length)];
+        menu_palavras.insertAdjacentHTML("beforeend", `
+        <span class="badge rounded-pill text-bg-${corAleatoria} menu-palavras-chave__palavra">
+            ${palavra.nome}
+        </span>`);
+    }
+    menu_palavras.insertAdjacentHTML('beforeend', `<button class="botao_adicionar">
+        <i class="bi bi-plus-lg"></i>
+    </button>`);
+
+    //Adicionar os produtos
+    
 }
 
 carregarMenu()
