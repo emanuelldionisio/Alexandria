69723f9e185102404f931089e583e26e87e2af1c
SamuelSLinhares <samuel.linhares@academico.ifpb.edu.br>
2025-05-03 14:58:10 +0000

    inserção de usuários e livros concluído, i aint happy, im felling glad i got sunshine

---
+++ frontend/js/data/livro.js
@@ -0,0 +1,45 @@
+[
+    {
+        "id_prod": 1,
+        "nome": "Crime e Castigo",
+        "valor": 2390, // 23.90
+        "condicao": "Bom - Manchas", 
+        "descricao": "Possui algumas manchas amareladas nas páginas devido à velhice, sem rasgos ou avarias maiores",
+        "autor": "Fiódor Dostoiévski",
+        "edicao": "5° - Antofágica",
+        "qtd_pag": 592
+    },
+
+    {
+        "id_prod": 2,
+        "nome": "Quarta Asa",
+        "valor": 4590, // 45.90
+        "condicao": "Excelente", 
+        "descricao": "Em estado de novo, venda por motivo de mudança",
+        "autor": "Rebecca Yarros",
+        "edicao": "2° - Planeta Minotauro",
+        "qtd_pag": 544
+    },
+
+    {
+        "id_prod": 3,
+        "nome": "A Canção de Aquiles",
+        "valor": 2260, // 22.60
+        "condicao": "Avariado", 
+        "descricao": "Possui marcas de uso na capa, como arranhões e amassados",
+        "autor": "Madeline Miller",
+        "edicao": "3° - Planeta Minotauro",
+        "qtd_pag": 392
+    },
+
+    {
+        "id_prod": 4,
+        "nome": "O Hobbit",
+        "valor": 2830, // 28.30
+        "condicao": "Excelente", 
+        "descricao": "Sem danos",
+        "autor": "J.R.R Tolkien",
+        "edicao": "3° - Harper Collins Brasil",
+        "qtd_pag": 331
+    }
+]
