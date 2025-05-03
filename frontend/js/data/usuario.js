5b96d0b43942d23905ac7da8e9ee1c6a383b79aa
SamuelSLinhares <samuel.linhares@academico.ifpb.edu.br>
2025-05-03 20:56:58 +0000

    gravity don't mean to much to me im who i got to be

--- frontend/js/data/usuario.js
+++ frontend/js/data/usuario.js
@@ -16,7 +16,7 @@ export const usuario = [
 
    {
         "cod": 2,
-        "nome": "Alice Batista",
+        "nome": "Samarinha",
         "tel1": "(83) 99352-8767",
         "tel2": null,
         "email": "alice.batista@academico.ifpb.edu.br",
@@ -27,5 +27,50 @@ export const usuario = [
         "bairro": "Geisel",
         "rua": "Rua Passarinho Bem-te-vi da Silva Assobiante",
         "num_casa": 479
-}
+    },
+
+    {
+        "cod": 3,
+        "nome": "Manuca",
+        "tel1": "(83) 98398-8428",
+        "tel2": null,
+        "email": "emanuele.targino@academico.ifpb.edu.br",
+        "dt_nascimento": "08/01/2008",
+        "pais": "Brasil",
+        "estado": "Paraíba",
+        "cidade": "João Pessoa",
+        "bairro": "Treze de Maio",
+        "rua": "Rua Samba Potente que Desce no Pé",
+        "num_casa": 791
+    },
+
+    {
+        "cod": 4,
+        "nome": "Çãm",
+        "tel1": "(83) 99404-6168",
+        "tel2": null,
+        "email": "samuel.linhares@academico.ifpb.edu.br",
+        "dt_nascimento": "14/11/2007",
+        "pais": "Brasil",
+        "estado": "Paraíba",
+        "cidade": "João Pessoa",
+        "bairro": "José Américo",
+        "rua": "Rua Padrinho Chico Xavier Astrólogo",
+        "num_casa": 331
+    },
+
+    {
+        "cod": 5,
+        "nome": "Oliveira",
+        "tel1": "(83) 94334-7790",
+        "tel2": null,
+        "email": "rodrigues.luzia@academico.ifpb.edu.br",
+        "dt_nascimento": "01/08/2007",
+        "pais": "Brasil",
+        "estado": "Paraíba",
+        "cidade": "João Pessoa",
+        "bairro": "Bancários",
+        "rua": "Rua Banqueiros em Protesto por Aumento Salarial",
+        "num_casa": 414
+    },
 ]
