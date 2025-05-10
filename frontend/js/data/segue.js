e1cb12ce7ac0b043156455af9de82a34ff5badb7
emanuelldionisio <emanuel.eduardo@academico.ifpb.edu.br>
2025-05-10 15:17:30 -0300

    Uni as funçoes comuns a menu.js e peril.js em menu_peril.js

--- frontend/js/data/segue.js
+++ frontend/js/data/segue.js
@@ -1,26 +1,26 @@
 export const segue = [
     { 
         "seguinte": 1, 
-        "seguindo": 2 
+        "seguido": 2 
     },
     { 
         "seguinte": 1, 
-        "seguindo": 3 
+        "seguido": 3 
     },
     { 
         "seguinte": 2, 
-        "seguindo": 4 
+        "seguido": 4 
     },
     { 
         "seguinte": 3, 
-        "seguindo": 1 
+        "seguido": 1 
     },
     { 
         "seguinte": 4, 
-        "seguindo": 5 
+        "seguido": 5 
     },
     { 
         "seguinte": 5, 
-        "seguindo": 3 
+        "seguido": 3 
     }
 ]
--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -1,68 +1,14 @@
 import { usuario } from './data/usuario.js';
-import { segue } from './data/segue.js';
-import { palavra_usuario } from './data/palavra_usuario.js';
-import { disco } from './data/disco.js';
-import { livro } from './data/livro.js';
+import { menu_perfil } from './menu_perfil.js';
 
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id_user");
 
 function carregarMenu() {
-    
-    const coresBootstrap = [
-        'primary', 'secondary', 'success', 'danger',
-        'warning', 'info', 'dark'
-    ];
-
-    //Ajustar a quantidade de seguidores
-    let container_seguidores = document.getElementById("menu-usuario__informacoes__seguidores");
-    container_seguidores.innerHTML = segue.filter(obj => obj.seguindo == id).length + " seguidores";
-
-    let container_segue = document.getElementById("menu-usuario__informacoes__seguindo");
-    container_segue.innerHTML = segue.filter(obj => obj.seguinte == id).length + " seguindo";
-
-    //Adicionar a foto de perfil
-    let img_fotodeperfil = document.getElementById("foto-de-perfil");
-    img_fotodeperfil.src = `imgs/usuario/${id}.jpg`;
-
+    menu_perfil(id);
     //Adicionar o nome do user
     let mensagem_boasvindas = document.getElementById("menu-usuario__mensagem");
     mensagem_boasvindas.innerHTML = `Olá, ${usuario.find(obj => obj.cod == id).nome}`;
-
-    //Adicionar as palavras chave
-    let palavras = palavra_usuario.filter(obj => obj.usuario == id);
-    let menu_palavras = document.getElementById("menu-palavras-chave__palavras-chave");
-    for (let palavra of palavras) {
-        let corAleatoria = coresBootstrap[Math.floor(Math.random() * coresBootstrap.length)];
-        menu_palavras.insertAdjacentHTML("beforeend", `<span class="badge rounded-pill text-bg-${corAleatoria} menu-palavras-chave__palavra">
-            ${palavra.nome}
-        </span>`);
-    }
-
-    //Adicionar os produtos
-    let meus_discos = disco.filter(obj => obj.id_usuario == id);
-    let meus_livros = livro.filter(obj => obj.id_usuario == id);
-    let container_produtos = document.getElementById("menu-produtos__produtos");
-
-    for (let i of meus_discos) {
-        container_produtos.insertAdjacentHTML("beforeend", `<div class="menu-produtos__produtos_produto" onclick="window.location.href = 'produto.html?id_prod=${i.id_prod}'">
-                <img src="imgs/prod/${i.id_prod}.jpg" class="card-img-top" alt="...">
-                <div>
-                    <h5>${i.nome}</h5>
-                    <h6>R$ ${(i.valor/100).toFixed(2)}</h6>
-                </div>
-            </div>`)
-    }
-
-    for (let i of meus_livros) {
-        container_produtos.insertAdjacentHTML("beforeend", `<div class="menu-produtos__produtos_produto" onclick="window.location.href = 'produto.html?id_prod=${i.id_prod}'">
-                <img src="imgs/prod/${i.id_prod}.jpg" class="card-img-top" alt="...">
-                <div>
-                    <h5>${i.nome}</h5>
-                    <h6>R$ ${(i.valor / 100).toFixed(2)}</h6>
-                </div>
-            </div>`)
-    }
 }
 
 const botao_ir_para_inicial = document.querySelector(".botao-pagina-inicial");
