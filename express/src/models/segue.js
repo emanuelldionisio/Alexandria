805057cc919481a64780e1d7cdb78f0ca2716f66
SamuelSLinhares <samuel.linhares@academico.ifpb.edu.br>
2025-06-24 21:19:49 +0000

    modulo segue pronto

--- express/src/models/segue.js
+++ express/src/models/segue.js
@@ -1,59 +1,89 @@
-import { data } from '../database/data.js';
-
-function remove({ seguido, seguinte }) {
-    if (!seguido || !seguinte) {
-        throw new Error("Os campos 'seguido' e 'seguinte' são obrigatórios");
-    }
-    const index = data.segue.findIndex((obj) => obj.seguido == seguido && obj.seguinte == seguinte);
-    if (index === -1) {
-        throw new Error('Relação de seguimento não existe');
-    }
-    data.segue.splice(index, 1);
-    return { seguido, seguinte };
-}
-
-function create({ seguido, seguinte }) {
-    if (!seguido || !seguinte) {
-        throw new Error("Os campos 'seguido' e 'seguinte' são obrigatórios");
-    }
-    if (data.segue.find((obj) => obj.seguido == seguido && obj.seguinte == seguinte)) {
-        throw new Error('Relação de seguimento já existe');
-    }
-    const newSeguimento = { seguido, seguinte };
-    data.segue.push(newSeguimento); 
-    return { seguido, seguinte };
-}
-
-function readSeguidores(id) {
-    const seguidores = data.segue.filter((obj) => obj.seguido == id ).map((obj) => { return obj.seguinte});
-    return seguidores;
-}
-function readSeguidos(id) {
-    const seguidos = data.segue.filter((obj) => obj.seguinte == id ).map((obj) => { return obj.seguido});
-    return seguidos;
-}
-
-export default { readSeguidores, remove, readSeguidos, create };
-
-import Database from './database.js';
+import Database from "../database/database.js";
 
 async function create ({seguinte, seguido}) {
     const db = await Database.connect();
 
-    if (seguinte && seguido) {
-        const sql = `
-            INSERT INTO
-                investments (seguinte, seguido)
-            VALUES
-                (?,?)
-        `
+    if (!seguinte || !seguido) {
+        throw new Error("Campos 'seguinte' e 'seguido' são obrigatórios");
     }
+    
+    const sql = `
+        INSERT INTO
+            investments (seguinte, seguido)
+        VALUES
+            (?,?)
+    `;
 
     const { lastID } = await db.run(sql, [seguinte, seguido]);
 
     return await readById(lastID);
-}   else {
-    throw new Error('Unable to create investment');
 }
 
-export default { up };
+async function remove ({seguinte, seguido}) {
+    const db = await Database.connect();
+
+    if (seguinte && seguido) {
+        const sql = `
+            DELETE FROM 
+                segue
+            WHERE  
+                seguido = ? AND seguinte = ?
+        `;
+    }
+
+    const { changes } = await db.run(sql, [seguinte, seguido]);
+
+    return changes
+}
+
+async function readSeguidores ({seguido}) {
+    const db = await Database.connect();
+
+    if (seguido) {
+        const sql = `
+            SELECT COUNT(*) FROM
+                segue
+            WHERE
+                seguido = ?`
+    }
+    
+    const quantidade = await db.get(sql, [seguido]);
+    
+    return quantidade;
+}
+
+async function readSeguidos ({seguinte}) {
+    const db = await Database.connect();
+
+    if (seguinte) {
+        const sql = `
+            SELECT COUNT(*) FROM
+                segue
+            WHERE
+                seguinte = ?`
+    }
+    
+    const quantidade = await db.get(sql, [seguinte]);
+    
+    return quantidade;
+}
+
+async function readById(id) {
+    const db = await Database.connect();
+
+    const sql = `
+        SELECT *
+        FROM investments
+        WHERE id = ?
+    `;
+
+    const result = await db.get(sql, [id]);
+
+    if (!result) {
+        throw new Error(`Registro com ID ${id} não encontrado`);
+    }
+
+    return result;
+}
+
+export default { create, remove, readSeguidores, readSeguidos, readById };
