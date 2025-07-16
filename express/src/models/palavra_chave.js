6ffeb550fdecc36c8d404e77ebe1620cec7f53b3
Emanuele <emanuele.targino@academico.ifpb.edu.br>
2025-07-16 20:59:58 -0300

    models de palavra-chave

--- express/src/models/palavra_chave.js
+++ express/src/models/palavra_chave.js
@@ -1,23 +1,25 @@
+import Database from '../database/database.js';
+
 async function create({nome}) {
     const db = await Database.connect();
 
     if (!nome) {
         throw new Error('Unable to create keyword');
-    }
-
+    } 
     const sql = `
         INSERT INTO
             palavra_chave (nome)
-        VALUES
+            VALUES
             (?)
-        `;                                                                            
+        `;
+    await db.run(sql, [nome]);
+    return {nome};
 }
 
-async function read(nome) {
-    if (!nome) {
-        throw new Error("O campo 'seguinte' é obrigatório");
+async function read_user(cod_usuario) {
+    if (!cod_usuario) {
+        throw new Error("O campo 'cod_usuario' é obrigatório");
     }
-
     const db = await Database.connect();
 
     const sql = `
@@ -25,10 +27,21 @@ async function read(nome) {
         FROM
             palavra_chave
         WHERE
-            nome = ?`
+            cod_usuario = ?`
 
-    const palavra= await db.all(sql, [nome]);
+    const palavra= await db.all(sql, [cod_usuario]);
     return palavra
 }
+async function read_all() {
+    const db = await Database.connect();
 
-export default { create, read }
+    const sql = `
+        SELECT *
+        FROM
+            palavra_chave`
+    
+    const palavras = await db.all(sql);
+    return palavras;
+}
+
+export default { create, read_user, read_all }
