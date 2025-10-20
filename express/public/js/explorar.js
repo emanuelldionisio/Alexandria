d913634818b39d04af30ee4623cd8fa726e9f521
Emanuele <emanuele.targino@academico.ifpb.edu.br>
2025-10-19 23:44:45 -0300

    mexidas no explorar

--- express/public/js/explorar.js
+++ express/public/js/explorar.js
@@ -1,73 +1,66 @@
-import usuario from "../../src/models/usuario";
+import Auth from "./lib/auth.js";
+import API from './services/api.js';
 
-const searchInput = document.getElementById("search");
-
-let janela_aberta = false;
 const params = new URLSearchParams(window.location.search);
-const id_user = params.get("id_user");
-const id = params.get("id_visitado");
 
-if ((!id || !id_user)) {
-    document.body.innerHTML = "<h1>Verifique se os parâmetros da url são válidos!</h1>";
-    throw new Error("Faltam parâmetros na URL: id_visitado e/ou id_user", 400);
+if (!Auth.isAuthenticated()) {
+    throw new Error("Usuário não autenticado", 400);
 }
 
-if (id == id_user) {
-    window.location.href = `menu.html?id_user=${id_user}`;
+const id = params.get("id");
+const foto_de_perfil = document.getElementById("foto-de-perfil");
+const modo = "excluir";
+let produtos = await API.read(`/usuario/me/${modo}/produtos_exibidos`);
+
+async function carregarInicial() { 
+   
+
+    // exibição de produtos
+    let grid_discos = document.getElementById("grid_discos");
+    let grid_livros = document.getElementById("grid_livros");
+
+    for (let i of produtos.discos) {
+        grid_discos.insertAdjacentHTML("beforeend", `
+            <div class="card-disco" 
+                onclick="window.location.href = 'produto.html?id_prod=${i.id_prod}&id_user=${id}&tipo=disco'">
+            <img src="imgs/discos/${i.id_prod}.jpg" 
+                alt="${i.nome}" 
+                class="card-img">
+            <div class="card-body">
+                <h5 class="card-title">${i.nome}</h5>
+            </div>
+            </div>
+        `);
 }
 
-const produtos = await fetch("data/produtoByUsuario", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        id_usuario: id_user,
-        modo: "excluir"
-    })
-}).then(response => response.json());
-
-const vendedores = await fetch(`data/vendedores?id_usuario=${id}`).then(response => response.json());
-
-// Evento de pesquisa
-searchInput.addEventListener("input", (e) => {
-    const query = e.target.value;
-    carregarExplorar(query);
-});
-
-function carregarExplorar(){
-    let container_produtos = document.getElementById("container-produtos");
-    let container_usuarios = document.getElementById("container-usuarios");
-
-    for (let livro of produtos.livros) {
-        container_produtos.insertAdjacentHTML("beforeend", `<div class="produto" onclick="window.location.href = 'produto.html?id_prod=${livro.id_prod}&id_user=${id_user}&tipo=livro'">
-                <img src="imgs/livros/${livro.id_prod}.jpg" class="card-img-top" alt="...">
-                <div>
-                    <h5>${livro.nome}</h5>
-                    <h6>R$ ${(livro.valor/100).toFixed(2)}</h6>
-                </div>
-            </div>`)
+    for (let i of produtos.livros) {
+        grid_livros.insertAdjacentHTML("beforeend", `
+            <div class="card-livro" 
+                onclick="window.location.href = 'produto.html?id_prod=${i.id_prod}&id_user=${id}&tipo=livro'">
+            <img src="imgs/livros/${i.id_prod}.jpg" 
+                alt="${i.nome}" 
+                class="card-img">
+            <div class="card-body">
+                <h5 class="card-title">${i.nome}</h5>
+            </div>
+            </div>
+        `);
     }
-
-    for (let disco of produtos.discos) {
-        container_produtos.insertAdjacentHTML("beforeend", `<div class="produto" onclick="window.location.href = 'produto.html?id_prod=${disco.id_prod}&id_user=${id_user}&tipo=disco'">
-                <img src="imgs/discos/${disco.id_prod}.jpg" class="card-img-top" alt="...">
-                <div>
-                    <h5>${disco.nome}</h5>
-                    <h6>R$ ${(disco.valor/100).toFixed(2)}</h6>
-                </div>
-            </div>`)
-    }
-
-    for (let vendedor of vendedores) {
-        container_usuarios.insertAdjacentHTML("beforeend", `<div class="vendedor">
-                <img src="imgs/usuario/${vendedor.id}.jpg" class="card-img-top" alt="...">
-                <div>
-                    <h5>${vendedor.nome}</h5>
-                </div>
-            </div>`)
-    }
-
 }
 
-carregarExplorar();
+export function irParaMenu() {
+    window.location.href = `menu.html?id_user=${id}`;
+}
+
+window.irParaMenu = irParaMenu;
+
+export function IrParaProduto() {
+    window.location.href = `.html?id_user=${id_prod}`;
+}
+
+window.IrParaProduto = IrParaProduto;
+
+carregarInicial();
+export function irParaInicial() {
+    window.location.href = `inicial.html?id_user=${id}`;
+}
